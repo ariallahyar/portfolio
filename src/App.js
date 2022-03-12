0fb39fec0825ae57285269257425a6272a6afa22
Arianna Allahyar <anallahyar@gmail.com>
2022-03-12 11:24:41 +0100

    FeatureProject component including styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Tech from './components/Tech';
+import FeatureProject from './components/FeatureProject';
 
 const App = () => {
   return (
@@ -12,6 +13,14 @@ const App = () => {
       <main>
         <About />
         <Tech />
+        <section className='section'>
+          <div class="container">
+            <h2>Featured Projects</h2>
+            <div class="project-group">
+              <FeatureProject />
+            </div>
+          </div>
+        </section>
       </main>
     </body>
   );
