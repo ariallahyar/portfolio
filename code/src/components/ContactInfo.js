2f33bd616d2462d991e2ddd02e1ae1f6f9ebc848
Arianna Allahyar <anallahyar@gmail.com>
2022-03-13 18:12:41 +0100

    Attempt to aligning contact links

--- code/src/components/ContactInfo.js
+++ code/src/components/ContactInfo.js
@@ -8,14 +8,16 @@ const ContactInfo = () => {
 				<div className="contact-text-group">
 					<h2>Get in touch</h2>
 					<p>Arianna Allahyar</p>
+					<div className='contact-links'>
 						<a href="mailto:anallahyar@gmail.com">Email</a>
 						&nbsp;&nbsp;•&nbsp;&nbsp;
 						<a href="https://www.linkedin.com/in/ariannaallahyar/">LinkedIn</a>
 						&nbsp;&nbsp;•&nbsp;&nbsp;
 						<a href="https://github.com/ariallahyar">GitHub</a>
+					</div>
 				</div>
 			</div>
-		</footer>
+		</footer >
 	);
 };
 
