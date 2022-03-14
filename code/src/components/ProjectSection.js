ba2409f3aa6a2f6eb6935d58daf900269466478b
Arianna Allahyar <anallahyar@gmail.com>
2022-03-14 18:12:22 +0100

    Extra space needed after exporting

--- code/src/components/ProjectSection.js
+++ code/src/components/ProjectSection.js
@@ -1,29 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const ProjectSection = (props) => {
-	return (
-		<section className='section'>
-			<div className="container">
+  return (
+    <section className="section">
+      <div className="container">
+        <h2>Featured Projects</h2>
 
-				<h2>Featured Projects</h2>
+        <div className="project-group">
+          {props.featureProjects.map((project, index) => (
+            <article key={index} className="project">
+              {project}
+            </article>
+          ))}
+        </div>
 
-				<div className="project-group">
-					{props.featureProjects.map((project, index) =>
-						<article key={index} className="project">
-							{project}
-						</article>)}
-				</div>
+        <h2 className="no-fill">Other projects</h2>
 
-				<h2 className="no-fill">Other projects</h2>
-
-				{props.projects.map((project, index) =>
-					<article key={index} className="project">
-						{project}
-					</article>)}
-			</div>
-			
-		</section>
-	);
+        {props.projects.map((project, index) => (
+          <article key={index} className="project">
+            {project}
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default ProjectSection;
